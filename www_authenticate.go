3161333ad4fac17cb1c44bfa1359a14517ae1e99
geliya <gly@geliya-deMacBook-Pro.local>
2022-08-11 14:29:46 +0800

    bugfix: support null string

--- www_authenticate.go
+++ www_authenticate.go
@@ -27,31 +27,31 @@ func newWwwAuthenticate(s string) *wwwAuthenticate {
 		wa.Algorithm = algorithmMatch[1]
 	}
 
-	domainRegex := regexp.MustCompile(`domain="(.+?)"`)
+	domainRegex := regexp.MustCompile(`domain="(.*?)"`)
 	domainMatch := domainRegex.FindStringSubmatch(s)
 	if domainMatch != nil {
 		wa.Domain = domainMatch[1]
 	}
 
-	nonceRegex := regexp.MustCompile(`nonce="(.+?)"`)
+	nonceRegex := regexp.MustCompile(`nonce="(.*?)"`)
 	nonceMatch := nonceRegex.FindStringSubmatch(s)
 	if nonceMatch != nil {
 		wa.Nonce = nonceMatch[1]
 	}
 
-	opaqueRegex := regexp.MustCompile(`opaque="(.+?)"`)
+	opaqueRegex := regexp.MustCompile(`opaque="(.*?)"`)
 	opaqueMatch := opaqueRegex.FindStringSubmatch(s)
 	if opaqueMatch != nil {
 		wa.Opaque = opaqueMatch[1]
 	}
 
-	qopRegex := regexp.MustCompile(`qop="(.+?)"`)
+	qopRegex := regexp.MustCompile(`qop="(.*?)"`)
 	qopMatch := qopRegex.FindStringSubmatch(s)
 	if qopMatch != nil {
 		wa.Qop = qopMatch[1]
 	}
 
-	realmRegex := regexp.MustCompile(`realm="(.+?)"`)
+	realmRegex := regexp.MustCompile(`realm="(.*?)"`)
 	realmMatch := realmRegex.FindStringSubmatch(s)
 	if realmMatch != nil {
 		wa.Realm = realmMatch[1]
@@ -63,7 +63,7 @@ func newWwwAuthenticate(s string) *wwwAuthenticate {
 		wa.Stale = (strings.ToLower(staleMatch[1]) == "true")
 	}
 
-	charsetRegex := regexp.MustCompile(`charset="(.+?)"`)
+	charsetRegex := regexp.MustCompile(`charset="(.*?)"`)
 	charsetMatch := charsetRegex.FindStringSubmatch(s)
 	if charsetMatch != nil {
 		wa.Charset = charsetMatch[1]
